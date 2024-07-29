28939b6bfa7a21c3725a4c03ef8b9a3075e4bf1a
Robson <robsonmoura970@hotmail.com>
2024-07-29 10:51:48 -0300

    First Commit

---
+++ mariwheater/src/main/java/br/com/mariwheater/mariwheater/MariwheaterApplication.java
@@ -0,0 +1,13 @@
+package br.com.mariwheater.mariwheater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariwheaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariwheaterApplication.class, args);
+	}
+
+}

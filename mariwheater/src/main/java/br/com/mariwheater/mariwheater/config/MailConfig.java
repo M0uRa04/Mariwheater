cb92ebde4dd5e7406db7c49afa78b85d6ed01c4c
Robson <robsonmoura970@hotmail.com>
2024-08-16 12:36:19 -0300

    Implementando a primeira lógica de envio de emails aos usuários

---
+++ mariwheater/src/main/java/br/com/mariwheater/mariwheater/config/MailConfig.java
@@ -0,0 +1,7 @@
+package br.com.mariwheater.mariwheater.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmailConfig {
+}
---
+++ mariwheater/src/main/java/br/com/mariwheater/mariwheater/service/mail/MailService.java
@@ -0,0 +1,2 @@
+package br.com.mariwheater.mariwheater.service.mail;public class MailService {
+}

055710f687c91aad34811d25d16ac4f521d45b06
Robson <robsonmoura970@hotmail.com>
2024-08-16 12:36:29 -0300

    Implementando a primeira lógica de envio de emails aos usuários

--- mariwheater/src/main/java/br/com/mariwheater/mariwheater/service/mail/MailService.java
+++ mariwheater/src/main/java/br/com/mariwheater/mariwheater/service/mail/MailService.java
@@ -1,2 +1,23 @@
-package br.com.mariwheater.mariwheater.service.mail;public class MailService {
+package br.com.mariwheater.mariwheater.service.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleEmail (String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("robsonmouradev970@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+    }
 }
--- mariwheater/src/main/java/br/com/mariwheater/mariwheater/service/scheduling/ScheduledService.java
+++ mariwheater/src/main/java/br/com/mariwheater/mariwheater/service/scheduling/ScheduledService.java
@@ -5,6 +5,7 @@ import br.com.mariwheater.mariwheater.external.WheaterAPIService;
 import br.com.mariwheater.mariwheater.model.City;
 import br.com.mariwheater.mariwheater.service.CityService;
 import br.com.mariwheater.mariwheater.service.NotificationsService;
+import br.com.mariwheater.mariwheater.service.mail.MailService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -17,13 +18,16 @@ public class ScheduledService {
     private final CityService cityService;
     private final NotificationsService notificationsService;
 
-    public ScheduledService (WheaterAPIService wheaterAPIService, CityService cityService, NotificationsService notificationsService) {
+    private final MailService mailService;
+
+    public ScheduledService (MailService mailService, WheaterAPIService wheaterAPIService, CityService cityService, NotificationsService notificationsService) {
         this.wheaterAPIService = wheaterAPIService;
         this.cityService = cityService;
         this.notificationsService = notificationsService;
+        this.mailService = mailService;
     }
     //@Scheduled(fixedRate = 3600000) -> One hour request
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(cron = "0 0 6,18 * * *") //De 12 em 12 horas
     public void fetchDataAndSaveCities () {
         cityService.deleteLastHourRecords();
         cityService.resetAutoIncrement();
@@ -33,9 +37,14 @@ public class ScheduledService {
         }
     }
 
-    @Scheduled(fixedRate = 61000)
+    @Scheduled(cron = "0 0 7,19 * * *") //De 12 em 12 horas
     public void checkWeatherAndNotify() {
         List<City> dangerousCities = cityService.getAllCitiesWithTemperatureIsDangerous();
         notificationsService.createAllNotifications(dangerousCities);
+    }
+
+    @Scheduled(fixedRate = 60000)
+    public void sendTestEmail () {
+            mailService.sendSimpleEmail("robsonmoura970@gmail.com", "Email teste ", "Deu certo");
     }
 }
